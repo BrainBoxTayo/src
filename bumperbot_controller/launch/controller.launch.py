06ce64e0108b792a72418bee5dd1237e8a1c2f0f
BrainboxTayo <temitayo.akanbi-bello@stu.cu.edu.ng>
2024-09-12 17:29:05 +0100

    added controller for the wheel velocities

--- bumperbot_controller/launch/controller.launch.py
+++ bumperbot_controller/launch/controller.launch.py
@@ -1,7 +1,31 @@
 from launch import LaunchDescription
 from launch_ros.actions import Node
+from launch.actions import DeclareLaunchArgument
+from launch.substitutions import LaunchConfiguration
+from launch.conditions import IfCondition, UnlessCondition
+
 
 def generate_launch_description():
+
+    use_python_arg = DeclareLaunchArgument(
+        "use_python",
+        default_value="True"
+    )
+
+    wheel_radius_arg = DeclareLaunchArgument(
+        "wheel_radius",
+        default_value="0.033"
+    )
+
+    wheel_separation_arg = DeclareLaunchArgument(
+        "wheel_separation",
+        default_value="0.17"
+    )
+
+    # read value of the use_python argument
+    use_python = LaunchConfiguration("use_python")
+    wheel_radius = LaunchConfiguration("wheel_radius")
+    wheel_separation = LaunchConfiguration("wheel_separation")
 
     joint_state_broadcaster_spawner = Node(
         package="controller_manager",
@@ -10,7 +34,15 @@ def generate_launch_description():
             "joint_state_broadcaster",
             "--controller-manager",
             "/controller_manager"
-            ]
+        ]
+    )
+
+    simple_controller_py = Node(
+        package="bumperbot_controller",
+        executable="simple_controller.py",
+        parameters=[{"wheel_radius": wheel_radius,
+                     "wheel_separation": wheel_separation}],
+        condition=IfCondition(use_python)
     )
     simple_controller = Node(
         package="controller_manager",
@@ -19,11 +51,14 @@ def generate_launch_description():
             "simple_velocity_controller",
             "--controller-manager",
             "/controller_manager"
-            ]
+        ]
     )
 
-
     return LaunchDescription([
+        use_python_arg,
+        wheel_radius_arg,
+        wheel_separation_arg,
         joint_state_broadcaster_spawner,
-        simple_controller
+        simple_controller,
+        simple_controller_py
     ])
