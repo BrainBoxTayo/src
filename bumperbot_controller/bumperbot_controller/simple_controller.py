bb69d0284068b1ee91076009ed97d84e518bce01
BrainBoxTayo <temitayo.akanbi-bello@stu.cu.edu.ng>
2024-09-25 17:39:27 +0100

    mistakenly closed docker

--- bumperbot_controller/bumperbot_controller/simple_controller.py
+++ bumperbot_controller/bumperbot_controller/simple_controller.py
@@ -7,7 +7,10 @@ from geometry_msgs.msg import TwistStamped
 from sensor_msgs.msg import JointState
 import numpy as np
 from rclpy.time import Time
+from nav_msgs.msg import Odometry
 from rclpy.constants import S_TO_NS
+from math import sin, cos
+from tf_transformations import quaternion_from_euler
 
 class simpleController(Node):
     def __init__(self):
@@ -27,6 +30,10 @@ class simpleController(Node):
         self.left_wheel_prev_pos = 0.0
         self.right_wheel_prev_pos = 0.0
         self.prev_time = self.get_clock().now()
+        
+        self.pos_x = 0.0
+        self.pos_y = 0.0
+        self.pos_theta = 0.0
 
         self.wheel_cmd_pub = self.create_publisher(
             Float64MultiArray, "simple_velocity_controller/commands", 10)
@@ -34,10 +41,22 @@ class simpleController(Node):
             TwistStamped, "bumperbot_controller/cmd_vel", self.velCallback, 10)
         self.joint_sub = self.create_subscription(
             JointState, "joint_states", self.jointCallback, 10)
+        self.odom_pub = self.create_publisher(
+            Odometry, "bumperbot_controller/odom", 10)
+        
 
         self.speed_conversion_matrix = np.array([[self.wheel_radius / 2, self.wheel_radius / 2],
                                                 [self.wheel_radius / self.wheel_separation, -(self.wheel_radius / self.wheel_separation)]])
 
+        self.odom_msg = Odometry()
+        self.odom_msg.header.frame_id = "odom"
+        self.odom_msg.child_frame_id = "base_footprint"
+        self.odom_msg.pose.pose.orientation.x = 0.0 
+        self.odom_msg.pose.pose.orientation.y = 0.0 
+        self.odom_msg.pose.pose.orientation.z = 0.0 
+        self.odom_msg.pose.pose.orientation.w = 1.0 
+        
+        
         self.get_logger().info("The conversion matrix is {}".format(self.speed_conversion_matrix))
 
     def velCallback(self, msg):
@@ -66,12 +85,29 @@ class simpleController(Node):
 
         phi_left = dp_left / ( dt.nanoseconds / S_TO_NS )
         phi_right = dp_right / ( dt.nanoseconds / S_TO_NS ) # Velocities of the wheels
+        
+        # self.get_logger().info("msg.position[1]: {}  msg.position[0]: {}".format(msg.position[1],msg.position[0]))
 
         linear_vel = (self.wheel_radius * phi_right + self.wheel_radius * phi_left) / 2 # From Kinematics
-        angular_vel = (self.wheel_radius * phi_right + self.wheel_radius * phi_left) / self.wheel_separation # From Kinematics
+        angular_vel = (self.wheel_radius * phi_right - self.wheel_radius * phi_left) / self.wheel_separation # From Kinematics
+        ds = (self.wheel_radius * phi_right + self.wheel_radius * phi_left) / 2
+        d_theta = (self.wheel_radius * phi_right - self.wheel_radius * phi_left) / self.wheel_separation
+        self.pos_theta += d_theta
+        self.pos_x += ds * cos(self.pos_theta)
+        self.pos_y += ds * sin(self.pos_theta)
 
-        self.get_logger().info("Linear_Velocity: {}, Angular_velocity: {}".format(linear_vel, angular_vel))
+        q = quaternion_from_euler(0, 0, self.pos_theta)
+        self.odom_msg.pose.pose.orientation.x = q[0]
+        self.odom_msg.pose.pose.orientation.y = q[1]
+        self.odom_msg.pose.pose.orientation.z = q[2]
+        self.odom_msg.pose.pose.orientation.w = q[3]
+        self.odom_msg.header.stamp = self.get_clock().now().to_msg()
+        self.odom_msg.pose.pose.position.x = self.pos_x
+        self.odom_msg.pose.pose.position.y = self.pos_y
+        self.odom_msg.twist.twist.linear.x = linear_vel
+        self.odom_msg.twist.twist.angular.z = angular_vel
 
+        self.odom_pub.publish(self.odom_msg)
 
 def main():
     rclpy.init()
